23bfaffeaf61c19fedd562f30e1565469d3eac82
bimurto <sksuter@gmail.com>
2018-08-11 22:13:06 +0600

    Initial commit

---
+++ src/main/java/com/bimurto/springBootTransactionalDemo/SpingTransactionalDemoApplication.java
@@ -0,0 +1,12 @@
+package com.bimurto.springBootTransactionalDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingTransactionalDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingTransactionalDemoApplication.class, args);
+    }
+}
