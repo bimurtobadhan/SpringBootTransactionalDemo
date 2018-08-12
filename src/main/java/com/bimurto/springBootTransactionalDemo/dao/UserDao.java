331533c311f87b8bbbfe8cf798e352286b608f52
Shawrup <sksuter@gmail.com>
2018-08-12 10:01:21 +0600

    Added test code for update

--- src/main/java/com/bimurto/springBootTransactionalDemo/dao/UserDao.java
+++ src/main/java/com/bimurto/springBootTransactionalDemo/dao/UserDao.java
@@ -1,9 +1,14 @@
 package com.bimurto.springBootTransactionalDemo.dao;
 
 import com.bimurto.springBootTransactionalDemo.domain.TestUser;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserDao {
     void saveWithSameClassTrxTransactionalMethodCall(TestUser user);
 
     void saveWithTransactionMethodWithException(TestUser user);
+
+    void updateWithTransactionMethodWithNoExplicitSave(String name);
+
+    void updateWithSameClassTrxTransactionalMethodCallWithNoExplicitSave(String name);
 }
